189c77ccfcbe16045d81e36aaa74fd9904ce78f6
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2019-03-24 19:11:27 +0300

    Data read protocol initialization.

--- TimsahGUI/Arduino/timsah-serial/timsah-serial.ino
+++ TimsahGUI/Arduino/timsah-serial/timsah-serial.ino
@@ -3,9 +3,33 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  Serial.write("45"); // send a byte with the value 45
-  Serial.write("\n");
-  delay(1000);
-  Serial.write("Timsah\n");
+  delay(2000);
+  
+  Serial.write("$\n");
+  Serial.write("TMP\n"); // Temp
+  Serial.write("#\n");
+
+  // Temp Data
+  Serial.write("25\n");
+  
+  Serial.write("$\n");
+  Serial.write("ACC\n"); // Acc
+  Serial.write("#\n");
+
+  // ACC Data
+  Serial.write("100\n");
+  Serial.write("200\n");
+  Serial.write("300\n");
+
+  Serial.write("$\n");
+  Serial.write("GYR\n"); // Gyr
+  Serial.write("#\n");
+
+  // GYR Data
+  Serial.write("-100\n");
+  Serial.write("220\n");
+  Serial.write("540\n");
+
+  // End of data
+  Serial.write("$$\n");
 }
--- TimsahGUI/NodeJS/index.js
+++ TimsahGUI/NodeJS/index.js
@@ -7,6 +7,53 @@ const port = new SerialPort('COM7', {
 
 const parser = port.pipe(new Readline({ delimiter: '\n' }))
 
+let status = 0
+let config = 0
+let mode = ''
+let rovTMP = ''
+let rovGYR = ''
+let rovACC = ''
+
 parser.on('data', function(data) {
-  console.log("Data: " + data);
+  if(data == '$') {
+    status = 1 // next data will be mode
+    mode = ''
+  }
+  // The command # means end of mode
+  else if(data == '#' && mode == 'ACC') {
+    status = 0
+    rovACC = '' // Clear ACC data befor write
+  }
+  else if(data == '#' && mode == 'GYR') {
+    status = 0
+    rovGYR = '' // Clear GYR data befor write
+  }
+  else if(data == '#' && mode == 'TMP') {
+    status = 0
+    rovTMP = '' // Clear TMP data befor write
+  }
+  // If end of data command ($$) received then print all data.
+  else if(data == '$$') {
+    console.log("TMP Data: " + rovTMP)
+    console.log("ACC Data: " + rovACC)
+    console.log("GYR Data: " + rovGYR)
+  }
+  // Get Mode
+  else if(status == 1) {
+    mode += data
+  }
+  // Get Data
+  else if(status == 0) {
+    if(mode == 'ACC') {
+      rovACC += data
+      rovACC += ','
+    }
+    if(mode == 'GYR') {
+      rovGYR += data
+      rovGYR += ','
+    }
+    if(mode == 'TMP') {
+      rovTMP = data
+    }
+  }
 })
