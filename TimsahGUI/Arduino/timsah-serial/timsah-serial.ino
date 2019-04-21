7681ec9e59463c42b86b827f8551c273dd75ea04
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2019-04-21 15:47:08 +0300

    Added new dadas to send.

--- TimsahGUI/Arduino/timsah-serial/timsah-serial.ino
+++ TimsahGUI/Arduino/timsah-serial/timsah-serial.ino
@@ -7,8 +7,22 @@ int tmp = 0;
 
 void loop() {
   
-  tmp++;
-  delay(3000);
+  tmp = random(25, 35);
+  
+  int Ax = random(-100,100);
+  int Ay = random(-100, 100);
+  int Az = random(-100, 100);
+
+  int Gx = random(-100,100);
+  int Gy = random(-100, 100);
+  int Gz = random(-100, 100);
+
+  int speedX = random(0,100);
+  int speedY = random(0,100);
+
+  int sonar = random(0, 400);
+  
+  delay(100);
   Serial.write("$\n");
   Serial.write("TMP\n"); // Temp
   Serial.write("#\n");
@@ -22,19 +36,43 @@ void loop() {
   Serial.write("#\n");
 
   // ACC Data
-  Serial.write("100\n");
-  Serial.write("200\n");
-  Serial.write("300\n");
+  Serial.print(Ax);
+  Serial.write("\n");
+  Serial.print(Ay);
+  Serial.write("\n");
+  Serial.print(Az);
+  Serial.write("\n");
 
   Serial.write("$\n");
   Serial.write("GYR\n"); // Gyr
   Serial.write("#\n");
 
   // GYR Data
-  Serial.write("-100\n");
-  Serial.write("220\n");
-  Serial.write("540\n");
+  Serial.print(Gx);
+  Serial.write("\n");
+  Serial.print(Gy);
+  Serial.write("\n");
+  Serial.print(Gz);
+  Serial.write("\n");
 
+  Serial.write("$\n");
+  Serial.write("SPD\n"); // Speed
+  Serial.write("#\n");
+
+  // Speed Data
+  Serial.print(speedX);
+  Serial.write("\n");
+  Serial.print(speedY);
+  Serial.write("\n");
+
+  Serial.write("$\n");
+  Serial.write("SON\n"); // Sonar
+  Serial.write("#\n");
+
+  // Speed Data
+  Serial.print(sonar);
+  Serial.write("\n");
+  
   // End of data
   Serial.write("$$\n");
 }
